9214e1175bd13fd474f09b34ae3c60b4b27ad8d4
kingmusung <abuga991008@gmail.com>
2024-09-29 19:53:43 +0900

    rabbitMQ page 라우팅

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RabbitMQPage from "./rabbitMQ/RabbitMQPage.tsx";
+import "./index.css";
+import RedisPage from "./redis/RedisPage.tsx";
+import MainPage from "./MainPage.tsx";
+import RabbitMQRestPage from "./rabbitMQ/RabbitMQRestPage.tsx";
+(window as any).global = window;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/rabbitmq" element={<RabbitMQPage />} />
+        <Route path="/rabbitmq/rest" element={<RabbitMQRestPage />} />
+        <Route path="/redis" element={<RedisPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  define: {
+    global: "globalThis", // globalThis는 브라우저와 Node.js 모두에서 사용 가능
+  },
+});
