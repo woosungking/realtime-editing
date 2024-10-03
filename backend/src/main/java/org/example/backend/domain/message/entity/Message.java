1b9e846bb336e138800eff375489543954f34567
woosungking <127114299+woosungking@users.noreply.github.com>
2024-10-04 03:24:27 +0900

    연관관계 수정 및 rabbitMQ => redis => DB 저장 로직 구현 및 확인

--- backend/src/main/java/org/example/backend/domain/message/entity/Message.java
+++ backend/src/main/java/org/example/backend/domain/message/entity/Message.java
@@ -1,12 +1,14 @@
 package org.example.backend.domain.message.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.example.backend.domain.room.entity.Room;
 
 @Entity
+@Getter
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +16,46 @@ public class Message {
 
     private String content;
     private String sender;
+    @ManyToOne
+    @JoinColumn(name="room_id")
+    @JsonBackReference
+    private Room room;
+
+    public static class Builder{
+        private Long id;
+
+        private String content;
+        private String sender;
+
+        private Room room;
+
+       public Builder setId(Long id){
+           this.id=id;
+           return this;
+       }
+       public Builder setContent(String content){
+           this.content=content;
+           return this;
+       }
+       public Builder setSender(String sender){
+           this.sender=sender;
+           return this;
+       }
+
+       public Builder setRoomId(Room room){
+           this.room=room;
+           return this;
+       }
+       public Message build(){
+           Message message = new Message();
+           message.id = this.id;
+           message.content=this.content;
+           message.sender=this.sender;
+           message.room=this.room;
+            return message;
+    }
 
     // Getter, Setter 등 필요한 메서드 추가
+}
+
 }
